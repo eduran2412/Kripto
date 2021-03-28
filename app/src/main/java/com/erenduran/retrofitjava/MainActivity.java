a4c61b8589aa62080452b7ccb4b7ab57367ddf16
TOSHIBA <57902145+eduran2412@users.noreply.github.com>
2021-03-28 22:17:56 +0300

    Api oluşturuldu

---
+++ app/src/main/java/com/erenduran/retrofitjava/MainActivity.java
@@ -0,0 +1,17 @@
+package com.erenduran.retrofitjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         // api oluşturuldu
+        //https://api.nomics.com/v1/prices?key=09b342bbcce01a8f095f65ba8f174f2a
+
+    }
+}
