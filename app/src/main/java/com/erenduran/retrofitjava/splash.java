c6955d2db4f26d6ac66cfc723d85e95c16801d37
TOSHIBA <57902145+eduran2412@users.noreply.github.com>
2021-05-23 00:35:09 +0300

    Splash screen uygulandı..

---
+++ app/src/main/java/com/erenduran/retrofitjava/splash.java
@@ -0,0 +1,29 @@
+package com.erenduran.retrofitjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.erenduran.retrofitjava.view.MainActivity;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+    }
+}
